5af8a1f35c658fd684d47df06dc064e4fecc4c43
Candy Lohse <candy.lohse@buschmais.com>
2025-02-20 13:45:49 +0100

    #29 Refactor: Fix jee-injection:NoCombinationOfBeanProducersAndApplicationCode
    - Add filter for constructors in constraint definition
    - Refactor: Rename test set class BeanProducer to BeanProducerWithConstraintViolations
    - Add test for BeanProducers without violation

--- src/test/java/org/jqassistant/plugin/jee/injection/test/set/BeanProducerWithConstraintViolations.java
+++ src/test/java/org/jqassistant/plugin/jee/injection/test/set/BeanProducerWithConstraintViolations.java
@@ -3,7 +3,7 @@ package org.jqassistant.plugin.jee.injection.test.set;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 
-public class BeanProducer {
+public class BeanProducerWithConstraintViolations {
 
     @Inject
     private Object injectionPointField;
