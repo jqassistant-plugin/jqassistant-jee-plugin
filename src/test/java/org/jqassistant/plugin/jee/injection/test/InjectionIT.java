836130134ece4c21ab0650d056a38f6f5344169f
Candy Lohse <candy.lohse@buschmais.com>
2025-04-01 10:46:29 +0200

    #33 jee-injection: Change BeansMustNotUseFieldInjectionExceptEJBs to BeansMustNotUseFieldInjection

--- src/test/java/org/jqassistant/plugin/jee/injection/test/InjectionIT.java
+++ src/test/java/org/jqassistant/plugin/jee/injection/test/InjectionIT.java
@@ -4,6 +4,8 @@ import com.buschmais.jqassistant.core.report.api.model.Result;
 import com.buschmais.jqassistant.core.report.api.model.Row;
 import com.buschmais.jqassistant.core.rule.api.model.Concept;
 import com.buschmais.jqassistant.core.rule.api.model.Constraint;
+import com.buschmais.jqassistant.plugin.java.api.model.FieldDescriptor;
+import com.buschmais.jqassistant.plugin.java.api.model.TypeDescriptor;
 import com.buschmais.jqassistant.plugin.java.test.AbstractJavaPluginIT;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.InstanceOfAssertFactories;
@@ -19,6 +21,8 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import static com.buschmais.jqassistant.core.report.api.model.Result.Status.SUCCESS;
+import static com.buschmais.jqassistant.plugin.java.test.assertj.FieldDescriptorCondition.fieldDescriptor;
+import static com.buschmais.jqassistant.plugin.java.test.assertj.TypeDescriptorCondition.typeDescriptor;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -244,33 +248,43 @@ public class InjectionIT extends AbstractJavaPluginIT {
     }
 
     /**
-     * Verifies the constraint "jee-injection:BeansMustNotUseFieldInjectionExceptEJBs".
-     *
-     * @throws IOException If the test fails.
+     * Verifies the constraint "jee-injection:BeansMustNotUseFieldInjection.
      */
     @Test
     void fieldInjectionWithEjb_Violation() throws Exception {
         scanClasses(BeanProducerWithConstraintViolations.class, LocalEjb.class);
-        Result<Constraint> constraintResult = validateConstraint("jee-injection:BeansMustNotUseFieldInjectionExceptEJBs");
+        Result<Constraint> constraintResult = validateConstraint("jee-injection:BeansMustNotUseFieldInjection");
         store.beginTransaction();
         assertThat(constraintResult.getStatus()).isEqualTo(Result.Status.FAILURE);
-        assertThat(constraintResult.getRows()).hasSize(1);
-        assertThat(constraintResult.getRows().get(0).getColumns().get("Type").getLabel()).endsWith("test.set.BeanProducerWithConstraintViolations");
+        assertThat(constraintResult.getRows()).hasSize(2);
+
+        final List<TypeDescriptor> types = constraintResult.getRows().stream()
+                .map(Row::getColumns)
+                .map(row -> row.get("Type").getValue())
+                .map(TypeDescriptor.class::cast)
+                .collect(Collectors.toList());
+        assertThat(types).haveExactly(2, typeDescriptor(BeanProducerWithConstraintViolations.class));
+
+        final List<FieldDescriptor> fields = constraintResult.getRows().stream()
+                .map(Row::getColumns)
+                .map(row -> row.get("Field").getValue())
+                .map(FieldDescriptor.class::cast)
+                .collect(Collectors.toList());
+        assertThat(fields).haveExactly(1, fieldDescriptor(BeanProducerWithConstraintViolations.class, "injectionPointField"));
+        assertThat(fields).haveExactly(1, fieldDescriptor(BeanProducerWithConstraintViolations.class, "ejb"));
+
         assertThat(constraintResult.getRows().get(0).getColumns().get("Field").getLabel()).endsWith("Object injectionPointField");
         store.commitTransaction();
     }
 
     /**
-     * Verifies the constraint "jee-injection:BeansMustNotUseFieldInjectionExceptEJBs" results in no
+     * Verifies the constraint "jee-injection:BeansMustNotUseFieldInjection results in no
      * violations when applied to beans with setter or constructor injection.
-     *
-     * @throws java.io.IOException If the test fails.
      */
     @Test
     void fieldInjectionWithEjb_No_Violation() throws Exception {
-        scanClasses(BeanWithConstructorInjection.class);
-        scanClasses(BeanWithSetterInjection.class);
-        String ruleName = "jee-injection:BeansMustNotUseFieldInjectionExceptEJBs";
+        scanClasses(BeanWithConstructorInjection.class, BeanWithSetterInjection.class);
+        String ruleName = "jee-injection:BeansMustNotUseFieldInjection";
         assertThat(validateConstraint(ruleName).getStatus()).isEqualTo(SUCCESS);
         store.beginTransaction();
 
