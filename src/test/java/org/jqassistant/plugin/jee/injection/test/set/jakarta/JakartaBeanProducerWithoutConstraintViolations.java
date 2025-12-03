d3704c2af7a3358ace276c5a9679994516618765
Moritz Pflügner <moritz.pfluegner45@gmail.com>
2025-12-03 12:48:43 +0100

    #70: support for bean producer delegation to private methods

--- src/test/java/org/jqassistant/plugin/jee/injection/test/set/jakarta/JakartaBeanProducerWithoutConstraintViolations.java
+++ src/test/java/org/jqassistant/plugin/jee/injection/test/set/jakarta/JakartaBeanProducerWithoutConstraintViolations.java
@@ -1,12 +1,24 @@
 package org.jqassistant.plugin.jee.injection.test.set.jakarta;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 
+@ApplicationScoped
 public class JakartaBeanProducerWithoutConstraintViolations {
 
     @Produces
     public JakartaInjectableA beanProducerA(){
         return new JakartaInjectableA();
     }
+
+    @Produces
+    public JakartaInjectableB beanProducerB(){
+        return instantiate();
+    }
+
+    private JakartaInjectableB instantiate() {
+        return new JakartaInjectableB();
+    }
+
 
 }
--- src/test/java/org/jqassistant/plugin/jee/injection/test/set/javax/JavaxBeanProducerWithoutConstraintViolations.java
+++ src/test/java/org/jqassistant/plugin/jee/injection/test/set/javax/JavaxBeanProducerWithoutConstraintViolations.java
@@ -1,12 +1,23 @@
 package org.jqassistant.plugin.jee.injection.test.set.javax;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 
+@ApplicationScoped
 public class JavaxBeanProducerWithoutConstraintViolations {
 
     @Produces
     public JavaxInjectableA beanProducerA(){
         return new JavaxInjectableA();
+    }
+
+    @Produces
+    public JavaxInjectableB beanProducerB(){
+        return instantiate();
+    }
+
+    private JavaxInjectableB instantiate(){
+        return new JavaxInjectableB();
     }
 
 }
