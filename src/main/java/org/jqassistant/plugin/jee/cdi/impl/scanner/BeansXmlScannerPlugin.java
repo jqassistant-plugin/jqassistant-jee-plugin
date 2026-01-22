ff7840874d56e139dd4c922f64c17d5896599332
Dirk Mahler <dirk.mahler@buschmais.com>
2026-01-22 17:07:24 +0100

    #76 updated beans_1_1.xsd to beans_5_0.xsd

--- src/main/java/org/jqassistant/plugin/jee/cdi/impl/scanner/BeansXmlScannerPlugin.java
+++ src/main/java/org/jqassistant/plugin/jee/cdi/impl/scanner/BeansXmlScannerPlugin.java
@@ -11,56 +11,55 @@ import com.buschmais.jqassistant.core.scanner.api.ScannerContext;
 import com.buschmais.jqassistant.core.scanner.api.ScannerPlugin.Requires;
 import com.buschmais.jqassistant.core.scanner.api.Scope;
 import com.buschmais.jqassistant.core.shared.xml.JAXBHelper;
-import com.buschmais.jqassistant.plugin.common.api.model.FileDescriptor;
+import com.buschmais.jqassistant.plugin.common.api.scanner.AbstractScannerPlugin;
 import com.buschmais.jqassistant.plugin.common.api.scanner.filesystem.FileResource;
 import com.buschmais.jqassistant.plugin.java.api.model.TypeDescriptor;
 import com.buschmais.jqassistant.plugin.java.api.scanner.JavaScope;
 import com.buschmais.jqassistant.plugin.java.api.scanner.TypeResolver;
-import com.buschmais.jqassistant.plugin.xml.api.scanner.AbstractXmlFileScannerPlugin;
+import com.buschmais.jqassistant.plugin.xml.api.model.XmlFileDescriptor;
 
-import org.jcp.xmlns.xml.ns.javaee.Alternatives;
-import org.jcp.xmlns.xml.ns.javaee.Beans;
-import org.jcp.xmlns.xml.ns.javaee.Decorators;
-import org.jcp.xmlns.xml.ns.javaee.Interceptors;
+import https.jakarta_ee.xml.ns.jakartaee.Beans;
 import org.jqassistant.plugin.jee.cdi.api.model.BeansXmlDescriptor;
 
-@Requires(FileDescriptor.class)
-public class BeansXmlScannerPlugin extends AbstractXmlFileScannerPlugin<BeansXmlDescriptor> {
+@Requires(XmlFileDescriptor.class)
+public class BeansXmlScannerPlugin extends AbstractScannerPlugin<FileResource, BeansXmlDescriptor> {
+
+    private static final String TARGET_NAMESPACE = "https://jakarta.ee/xml/ns/jakartaee";
 
     private JAXBHelper<Beans> unmarshaller;
 
     @Override
     public void initialize() {
-        unmarshaller = new JAXBHelper<>(Beans.class);
+        unmarshaller = new JAXBHelper<>(Beans.class, TARGET_NAMESPACE);
     }
 
     @Override
-    public boolean accepts(FileResource item, String path, Scope scope) throws IOException {
+    public boolean accepts(FileResource item, String path, Scope scope) {
         return JavaScope.CLASSPATH.equals(scope) && ("/META-INF/beans.xml".equals(path) || "/WEB-INF/beans.xml".equals(path));
     }
 
     @Override
-    public BeansXmlDescriptor scan(FileResource item, BeansXmlDescriptor beansXmlDescriptor, String path, Scope scope, Scanner scanner) throws IOException {
+    public BeansXmlDescriptor scan(FileResource item, String path, Scope scope, Scanner scanner) throws IOException {
         ScannerContext context = scanner.getContext();
+        XmlFileDescriptor xmlFileDescriptor = context.getCurrentDescriptor();
+        BeansXmlDescriptor beansXmlDescriptor = context.getStore()
+                .addDescriptorType(xmlFileDescriptor, BeansXmlDescriptor.class);
         Beans beans = unmarshal(item);
         beansXmlDescriptor.setVersion(beans.getVersion());
         beansXmlDescriptor.setBeanDiscoveryMode(beans.getBeanDiscoveryMode());
-        for (Object o : beans.getInterceptorsOrDecoratorsOrAlternatives()) {
-            if (o instanceof Interceptors) {
-                addTypes(((Interceptors) o).getClazz(), beansXmlDescriptor.getInterceptors(), context);
-            } else if (o instanceof Decorators) {
-                addTypes(((Decorators) o).getClazz(), beansXmlDescriptor.getDecorators(), context);
-            } else if (o instanceof Alternatives) {
-                List<JAXBElement<String>> clazzOrStereotype = ((Alternatives) o).getClazzOrStereotype();
-                for (JAXBElement<String> element : clazzOrStereotype) {
-                    TypeDescriptor alternative = scanner.getContext()
-                            .peek(TypeResolver.class)
-                            .resolve(element.getValue(), context)
-                            .getTypeDescriptor();
-                    beansXmlDescriptor.getAlternatives()
-                            .add(alternative);
-                }
-            }
+        addTypes(beans.getInterceptors()
+                .getClazz(), beansXmlDescriptor.getInterceptors(), context);
+        addTypes(beans.getDecorators()
+                .getClazz(), beansXmlDescriptor.getDecorators(), context);
+        List<JAXBElement<String>> clazzOrStereotype = beans.getAlternatives()
+                .getClazzOrStereotype();
+        for (JAXBElement<String> element : clazzOrStereotype) {
+            TypeDescriptor alternative = scanner.getContext()
+                    .peek(TypeResolver.class)
+                    .resolve(element.getValue(), context)
+                    .getTypeDescriptor();
+            beansXmlDescriptor.getAlternatives()
+                    .add(alternative);
         }
         return beansXmlDescriptor;
     }
